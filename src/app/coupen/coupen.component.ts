5a0b684ebb34e00421fec14a1eec4c05fc83aad3
Godwin Munan <godwinjethro@gmail.com>
2023-06-26 17:54:22 +0100

    init commit

---
+++ src/app/coupen/coupen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coupen',
+  templateUrl: './coupen.component.html',
+  styleUrls: ['./coupen.component.scss']
+})
+export class CoupenComponent {
+
+}
